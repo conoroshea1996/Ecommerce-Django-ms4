7795eb4b5b218b801fc18b8bf9fa4f768e00911d
conoroshea1996 <conoshea12@gmail.com>
2019-08-22 13:33:47 +0100

    project setup and create registration app

---
+++ ecomm/urls.py
@@ -0,0 +1,28 @@
+"""ecomm URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path, include
+from accounts import urls as urls_accounts
+from products import urls as urls_products
+from cart import urls as urls_carts
+from products.views import all_products
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^$', all_products, name='index'),
+    re_path(r'^accounts/', include(urls_accounts)),
+    re_path(r'^cart/', include(urls_carts)),
+]
