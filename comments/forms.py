9d95fd3830cfb69380644d830aa660115d954264
conoroshea1996 <conoshea12@gmail.com>
2019-09-03 15:34:36 +0100

    updated form to use drop down menu

--- comments/forms.py
+++ comments/forms.py
@@ -5,5 +5,16 @@ from .models import Comment
 class Message(forms.ModelForm):
     class Meta:
         model = Comment
-        widgets = {'date_posted': forms.HiddenInput()}
+        rating_choices = (
+            ('', 'Rating'),
+            (1, '1'),
+            (2, '2'),
+            (3, '3'),
+            (4, '4'),
+            (5, '5')
+        )
+        widgets = {
+            'date_posted': forms.HiddenInput(),
+            'rating': forms.Select(choices=rating_choices, attrs={'class': 'form-control'})
+        }
         fields = ('comment', 'rating', 'product')
